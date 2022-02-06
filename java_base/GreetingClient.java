226fc6f385cf4177abb9796c5ba9f5c27fc462f3
yourongL <1739180763@qq.com>
2022-02-06 13:39:58 +0800

    commit by LP

--- java_base/GreetingClient.java
+++ java_base/GreetingClient.java
@@ -10,25 +10,22 @@ import java.net.UnknownHostException;
 import java.nio.file.InvalidPathException;
 
 public class GreetingClient {
-    public static void main(String [] args) throws UnknownHostException, IOException
-    {
+    public static void main(String[] args) throws UnknownHostException, IOException {
         String serverName = args[0];
         int port = Integer.parseInt(args[1]);
-        try
-        {
-            System.out.println("连接到主机" + serverName + " ，端口号" + port);
+        try {
+            System.out.println("试图连接到服务器" + serverName + "  端口号" + port);
             Socket client = new Socket(serverName, port);
-            System.out.println("远程主机地址：" + client.getRemoteSocketAddress());
+            System.out.println("服务器IP是" + client.getRemoteSocketAddress());
             OutputStream outToServer = client.getOutputStream();
             DataOutputStream out = new DataOutputStream(outToServer);
-    
-            out.writeUTF("Hello from " + client.getLocalSocketAddress());
+
+            out.writeUTF("Hello from 客户端" + client.getLocalSocketAddress());
             InputStream inFromServer = client.getInputStream();
             DataInputStream in = new DataInputStream(inFromServer);
-            System.out.println("服务器响应 " + in.readUTF());
+            System.out.println("来自服务器的响应 " + in.readUTF());
             client.close();
-        }catch(InvalidPathException e)
-        {
+        } catch (InvalidPathException e) {
             e.printStackTrace();
         }
     }
--- java_base/GreetingServer.java
+++ java_base/GreetingServer.java
@@ -7,49 +7,41 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
 
-public class GreetingServer extends Thread{
-    private ServerSocket serverSocket;
-   
-   public GreetingServer(int port) throws IOException
-   {
+public class GreetingServer extends Thread {
+   private ServerSocket serverSocket;
+
+   public GreetingServer(int port) throws IOException {
       serverSocket = new ServerSocket(port);
       serverSocket.setSoTimeout(10000);
    }
- 
-   public void run()
-   {
-      while(true)
-      {
-         try
-         {
-            System.out.println("等待远程连接端口号为" + serverSocket.getLocalPort() + "...");
-            Socket server = serverSocket.accept();
-            System.out.println("远程主机地址" + server.getRemoteSocketAddress());
+
+   public void run() {
+      while (true) {
+         try {
+            System.out.println("服务器等待端口号为" + serverSocket.getLocalPort() + "...");
+            Socket server = serverSocket.accept();// 等待客户端来连接
+            System.out.println("来自客户端的IP" + server.getRemoteSocketAddress());
             DataInputStream in = new DataInputStream(server.getInputStream());
             System.out.println(in.readUTF());
             DataOutputStream out = new DataOutputStream(server.getOutputStream());
-            out.writeUTF("connect!" + server.getLocalSocketAddress() + "\nGoodbye!");
+            out.writeUTF("客户端你连上我这个服务器了, " + server.getLocalSocketAddress() + "\nGoodbye!");
             server.close();
-         }catch(SocketTimeoutException s)
-         {
+         } catch (SocketTimeoutException s) {
             System.out.println("Socket timed out!");
             break;
-         }catch(IOException e)
-         {
+         } catch (IOException e) {
             e.printStackTrace();
             break;
          }
       }
    }
-   public static void main(String [] args)
-   {
+
+   public static void main(String[] args) {
       int port = Integer.parseInt(args[0]);
-      try
-      {
+      try {
          Thread t = new GreetingServer(port);
          t.run();
-      }catch(IOException e)
-      {
+      } catch (IOException e) {
          e.printStackTrace();
       }
    }
